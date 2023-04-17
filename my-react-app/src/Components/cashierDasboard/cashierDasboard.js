75157d867b21f1a5734acc0d992326c4f86a0f08
Lorenzo Colendres <lorenzcolendres123@gmail.com>
2023-04-17 23:26:37 +0800

    cashierDashboard

---
+++ my-react-app/src/Components/cashierDasboard/cashierDasboard.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { GoPlus } from "react-icons/go";
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import './adminDashboard.css'
+
+
